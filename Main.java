ff161aa45cb3864ea7e8abcdd660f576e8b3f113
Arunava-27 <drarunkundu22@gmail.com>
2023-12-16 16:48:00 +0530

    added comments

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         
+        // Create two Tree objects
         Tree tree1 = new Tree(120, 12, TreeType.OAK);
         Tree tree2 = new Tree(80, 8, TreeType.MAPLE);
 
+        // call the announceTallTree method on each Tree object
         tree1.announceTallTree();
         tree2.announceTallTree();
     }
--- Tree.java
+++ Tree.java
@@ -1,19 +1,24 @@
 public class Tree {
+
+    // Add the instance variables here
     double heightFt;
     double trunkDiameterIn;
-    TreeType treeType;
+    TreeType treeType; // TreeType is an enum
 
+    // Add the constructor here
     public Tree(double heightFt, double trunkDiameterIn, TreeType treeType) {
         this.heightFt = heightFt;
         this.trunkDiameterIn = trunkDiameterIn;
         this.treeType = treeType;
     }
 
+    // Add the grow method here
     void grow() {
         this.heightFt += 9;
         this.trunkDiameterIn += 1;
     }
 
+    // Add the announceTallTree method here
     void announceTallTree() {
         if (this.heightFt > 100){
             System.out.println("That's a tall "+this.treeType+" tree!");
